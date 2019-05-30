2ede93ed284a33a2269bb656250a3b18fed4311c
Isi321 <50715355+Isi321@users.noreply.github.com>
2019-05-30 15:40:07 +0200

    Add files via upload

---
+++ Beispiel_GUI1/src/application/Beispiel_GUI1Controller.java
@@ -0,0 +1,85 @@
+package application;
+
+import java.awt.Polygon;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Beispiel_GUI1Controller {
+	@FXML
+	private Circle house1Black;
+	@FXML
+	private Circle house2Black;
+	@FXML
+	private Circle house3Black;
+	@FXML
+	private Circle house4Black;
+	@FXML
+	private Circle aim2Yellow;
+	@FXML
+	private Circle aim3Yellow;
+	@FXML
+	private Circle aim4Yellow;
+	@FXML
+	private Circle house1Red;
+	@FXML
+	private Circle house2Red;
+	@FXML
+	private Circle house4Red;
+	@FXML
+	private Circle house3Red;
+	@FXML
+	private Circle aim1Green;
+	@FXML
+	private Circle aim4Red;
+	@FXML
+	private Circle aim3Red;
+	@FXML
+	private Circle aim2Red;
+	@FXML
+	private Circle aim1Red;
+	@FXML
+	private Circle aim1Yellow;
+	@FXML
+	private Circle aim1Black;
+	@FXML
+	private Circle aim2Black;
+	@FXML
+	private Circle aim3Black;
+	@FXML
+	private Circle aim4Black;
+	@FXML
+	private Circle house4Green;
+	@FXML
+	private Circle house3Green;
+	@FXML
+	private Circle aim4Green;
+	@FXML
+	private Circle aim3Green;
+	@FXML
+	private Circle aim2Green;
+	@FXML
+	private Circle house2Yellow;
+	@FXML
+	private Circle house3Yellow;
+	@FXML
+	private Circle house4Yellow;
+	@FXML
+	private Circle house1Green;
+	@FXML
+	private Circle house2Green;
+	@FXML
+	private Circle house1Yellow;
+	@FXML
+	private Button wuerfeln;
+	@FXML
+	private Button ziehen;
+	@FXML
+	private Button naechsterSpieler;
+	
+	
+
+}
