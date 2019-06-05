29bbc0d301cdc0521b8810608c13828d2c881c9a
Isi321 <50715355+Isi321@users.noreply.github.com>
2019-06-05 15:09:51 +0200

    Add files via upload

---
+++ TestProject/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("BoardGUI.fxml"));
+        Parent root = fxmlLoader.load();
+        BoardGUIController gameController = fxmlLoader.getController();
+        gameController.initSelectUI();
+
+        primaryStage.setTitle("Mensch ärgere Dich nicht");
+        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.show();
+    }
+
+     
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
